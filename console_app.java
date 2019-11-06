56dd5c86d514e28e6c385fda47d610f25830710e
bananaenoch <52139187+bananaenoch@users.noreply.github.com>
2019-11-05 19:26:02 -0500

    Add files via upload

---
+++ console_app.java
@@ -0,0 +1,22 @@
+import java.util.Scanner; // imports scanner class
+
+public class console_app {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in); //create scanner object
+		System.out.println("Enter name: "); // asks for name input
+		
+		String name = input.nextLine(); // assigns input to variable
+				
+		System.out.println("Enter a noun (doesn't start with a vowel): "); // asks for noun input
+				
+		String noun = input.nextLine(); // assigns input to variable
+		System.out.println(name + " loves eating " + noun + "s."); // prints final sentence
+		
+		
+		System.out.println("Thanks for playing and control your diet! :)");
+	}
+
+}
